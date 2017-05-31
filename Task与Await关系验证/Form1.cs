23446067271d81fc3deee51debc3b1f2f7f6ed72
yangywtrue <1141583038@qq.com>
2017-05-31 16:25:52 +0800

    新增Task研究

---
+++ Task与Await关系验证/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task与Await关系验证
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Text = "Doing Stuff";
+
+            await Task.Delay(4000);
+            await Task.Delay(3000);
+            //await Task.Delay(3000);
+
+            textBox1.Text = "Not Doing Anything";
+            button1.Enabled = !button1.Enabled;
+
+            //await test();
+        }
+
+
+        //private async Task<int> test()
+        //{
+        //    Thread.Sleep(10000);
+        //    return 1;
+        //}
+    }
+}
---
+++ Task与Await关系验证/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task与Await关系验证
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Form1 form = new Form1();
+            //form.ShowDialog();
+
+            //Console.WriteLine("主线程测试开始..");
+            //AsyncMethod();
+            //Thread.Sleep(1000);
+            //Console.WriteLine("主线程测试结束..");
+            //Console.ReadLine();
+
+            Test();
+            Console.WriteLine("ssssssssssssssssss");
+            Console.ReadKey();
+        }
+
+        static async void Test()
+        {
+            Console.WriteLine("主线程测试开始..");
+            await AsyncMethod();
+            Thread.Sleep(1000);
+            Console.WriteLine("主线程测试结束..");
+            Console.ReadLine();
+        }
+
+
+        static async Task AsyncMethod()
+        {
+            Console.WriteLine("开始异步代码");
+            var result = await MyMethod();
+            Console.WriteLine("异步代码执行完毕");
+        }
+
+        static async Task<int> MyMethod()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("异步执行" + i.ToString() + "..");
+                await Task.Delay(1000); //模拟耗时操作
+            }
+            return 0;
+        }
+    }
+}
